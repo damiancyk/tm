2310081410fb8b7c406034a37ce5a51e0795c75d
Damian Pieta <damiancyk@gmail.com>
2016-07-31 23:15:17 +0200

    New angular modules

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,22 +1,14 @@
-var app = angular.module('myApp', [ 'ngRoute', 'toaster' ]);
+var app = angular.module('dmc', [ 'ngRoute']);
 
-app.config([ '$routeProvider', '$locationProvider', '$httpProvider',
-		function($routeProvider, $locationProvider, $httpProvider) {
-			$httpProvider.interceptors.push('httpErrorResponseInterceptor');
+app.config([ '$routeProvider',
+		function($routeProvider) {
+// $httpProvider.interceptors.push('httpErrorResponseInterceptor');
 			$routeProvider.when('/', {
-				title : 'Modules',
 				templateUrl : 'templates/modules.html',
-				controller : 'modulesCtrl',
-				access : {
-					public : false
-				}
+				controller : 'modulesCtrl'
 			}).when('/error', {
-				title : 'error',
 				templateUrl : 'templates/errors/error.html',
-				controller : 'modulesCtrl',
-				access : {
-					public : false
-				}
+				controller : 'modulesCtrl'
 			}).otherwise({
 				redirectTo : '/logout'
 			});
