3135d4da32c50a76af39fcbdb02858a9c54acc1f
Damian Pieta <damiancyk@gmail.com>
2016-08-03 17:14:52 +0200

    Simple angular routing Examples of bootstrap controls

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,15 +1,17 @@
-var app = angular.module('dmc', [ 'ngRoute']);
+var app = angular.module('tm', [ 'ngRoute' ]);
 
-app.config([ '$routeProvider',
-		function($routeProvider) {
-// $httpProvider.interceptors.push('httpErrorResponseInterceptor');
-			$routeProvider.when('/', {
-				templateUrl : 'templates/modules.html',
-				controller : 'modulesCtrl'
-			}).when('/error', {
-				templateUrl : 'templates/errors/error.html',
-				controller : 'modulesCtrl'
-			}).otherwise({
-				redirectTo : '/logout'
-			});
-		} ]);
+app.config([ '$routeProvider', function($routeProvider) {
+	// $httpProvider.interceptors.push('httpErrorResponseInterceptor');
+	$routeProvider.when('/item/list', {
+		templateUrl : 'itemList.html',
+		controller : 'itemListCtrl'
+	}).when('/', {
+		templateUrl : 'itemList.html',
+		controller : 'itemListCtrl'
+	}).when('/example-form', {
+		templateUrl : 'exampleForm.html',
+		controller : 'exampleFormCtrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+} ]);
---
+++ src/main/resources/static/app/itemCtrl.js
@@ -0,0 +1,3 @@
+app.controller('itemListCtrl', function($scope) {
+
+});
